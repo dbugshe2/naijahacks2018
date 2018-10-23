2b9ecd20c03f18a878a504e196a830ddaf8ec115
Maroof Shittu <dbugshe2@gmail.com>
2018-10-23 19:47:59 +0100

    complete create project page

--- js/demo.js
+++ js/demo.js
@@ -1,1 +1,19 @@
 //  just going to cram everything in here
+
+// projects sample model
+
+var project = {
+    id: "",
+    name: "",
+    type: "",
+    desc: "",
+    collaborator: "",
+    city: "",
+    state: "",
+    school: "",
+    userType: "",
+    costEstimate: "",
+    start: "",
+    end: ""
+
+};
