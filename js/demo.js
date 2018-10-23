fb804efd01dae74e7cf81f0c6ec8cf0e832d682e
Maroof Shittu <dbugshe2@gmail.com>
2018-10-23 20:32:34 +0100

    fix up form for script

--- js/demo.js
+++ js/demo.js
@@ -3,7 +3,7 @@
 // projects sample model
 
 var project = {
-    id: "",
+    id: 1,
     name: "",
     type: "",
     desc: "",
@@ -16,4 +16,43 @@ var project = {
     start: "",
     end: ""
 
-};
+},
+
+name = document.getElementById("projname"),
+projtype = document.getElementById("projtype"),
+projdesc = document.getElementById("projdesc"),
+projcollab = document.getElementById("projcollab"),
+projstate = document.getElementById("projstate"),
+usertype = document.getElementById("usertype"),
+cost = document.getElementById("cost"),
+start = document.getElementById("start"),
+end = document.getElementById("end"),
+submitbtn = document.getElementById("createsubmit");
+
+var createcontent = [name, projtype, projdesc, projcollab, projstate, usertype, cost, start, end];
+
+function populateProject(){
+
+}
+
+function createProject(){
+    return;
+}
+
+function saveProjects(){
+    return;
+}
+
+function validate(elList) {
+    elList.forEach(function ('element') {
+        if (element.value == null || element.value == ''){
+            element.className += " " + "is-invalid";
+        }
+    });
+}
+
+submitbtn.addEventListener('click', function(event) {
+    console.log("submited");
+    validate(createcontent);
+
+})
